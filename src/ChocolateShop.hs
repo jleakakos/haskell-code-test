ba46d6da66978e6757d8ed4780d4eafaa879b7c9
Jeremy Leakakos <jleakakos@stridenyc.com>
2015-09-08 10:06:40 -0400

    Start looking at a different way of redeeming which can handle the overall and optional limits

--- src/ChocolateShop.hs
+++ src/ChocolateShop.hs
@@ -2,7 +2,7 @@
 
 module ChocolateShop where
 
-import Data.Map (elems, foldWithKey, fromList, toList, insert, lookup, map, mapWithKey, Map)
+import Data.Map (elems, foldWithKey, fromList, toList, insert, insertWithKey, lookup, map, mapWithKey, Map)
 import Prelude hiding (lookup)
 import Data.Maybe (fromJust)
 import Data.Text hiding (all, foldl, filter)
@@ -27,6 +27,7 @@ runOrder (Order cash price wrappersNeeded chocolateType) = formattedOrder
         pre = Prelude.map (\(c,n) -> ((pack $ show c) `append` " " `append` (pack $ show n))) $ toList output
 
 emptyBasket = fromList $ Prelude.map (\t -> (t, 0)) [Milk ..]
+emptyBasketWithWrappers = Data.Map.map (\v -> (v,v)) emptyBasket
 
 purchaseChocolate :: Cash -> Price -> ChocolateType -> Map ChocolateType Int
 purchaseChocolate cash price chocolateType = insert chocolateType total emptyBasket
@@ -53,3 +54,24 @@ updateBasket basket wrappersNeeded chocolateType
         (darkTotal, darkWrappers) = fromJust $ lookup Dark basket
         (redeemedChocolates, unredeemedWrappers) = wrappersForType `divMod` wrappersNeeded
         (totalForType, wrappersForType) = fromJust $ lookup chocolateType basket 
+
+-- The following functions are a start at a different way of implementing
+-- redeeming
+redeemOneAtATime :: Map ChocolateType (Int, Int) -> WrappersNeeded -> ChocolateType -> Map ChocolateType (Int, Int)
+redeemOneAtATime basket wrappersNeeded chocolateType
+  | wrappersForType >= wrappersNeeded = redeemOneAtATime updatedBasket wrappersNeeded chocolateType
+  | otherwise = basket
+  where (totalForType, wrappersForType) = fromJust $ lookup chocolateType basket
+        updatedBasket = runPromotionForOneAtATime (insert chocolateType (totalForType + 1, wrappersForType - wrappersNeeded) basket) chocolateType
+
+runPromotionForOneAtATime :: Map ChocolateType (Int, Int) -> ChocolateType -> Map ChocolateType (Int, Int)
+runPromotionForOneAtATime basket chocolateType =
+  case lookup chocolateType promotions of
+    Just promotion -> promotion basket
+    Nothing        -> basket
+
+promotions :: Map ChocolateType (Map ChocolateType (Int, Int) -> Map ChocolateType (Int, Int))
+promotions = fromList [(Milk, milkPromotion), (White, whitePromotion), (SugarFree, sugarFreePromotion)]
+  where milkPromotion = insertWithKey (\k (total, wrappers) (t,w) -> (total + t, wrappers + w)) SugarFree (1,1)
+        whitePromotion = milkPromotion
+        sugarFreePromotion = insertWithKey (\k (total, wrappers) (t,w) -> (total + t, wrappers + w)) Dark (1,1)
